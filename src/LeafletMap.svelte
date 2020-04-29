a96d3a674be4ce5b02a3e509a07b53386a16c3d8
El Sonny <sonny@spaceshiplabs.com>
2020-04-29 13:21:06 -0500

    cases alpha

--- src/LeafletMap.svelte
+++ src/LeafletMap.svelte
@@ -1,6 +1,7 @@
 <script>
 	import L from 'leaflet';
 	import { onMount, getContext } from 'svelte';
+  import * as utils from "utils.js";
 
   let mapContainer;    
   let leafletMap;    
@@ -8,21 +9,25 @@
 	export let center = [23.6345, -102.5528];
   export let zoom = 5;
 
-    onMount(() => {
-        leafletMap = L.map(mapContainer).setView(center, zoom);
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-            attribution: '',
-            maxZoom: 18,
-            id: 'elsonny/ck90ay76y01yc1imzo80yen8t',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: 'pk.eyJ1IjoiZWxzb25ueSIsImEiOiJjazkwYWQ2d28wMDJ4M25vNjR3b2h5bWpiIn0.iQk1NtwS-2bJafmWg5Ol9w'
-        }).addTo(leafletMap);         
-        resize();
-    });    
-    const resize = () => {
-        leafletMap.invalidateSize();
-    };
+ 
+
+  onMount(async () => {
+      leafletMap = L.map(mapContainer).setView(center, zoom);
+      const markers = await utils.getMarkers();
+      markers.addTo(leafletMap);
+      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+          attribution: '',
+          maxZoom: 18,
+          id: 'elsonny/ck90ay76y01yc1imzo80yen8t',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: 'pk.eyJ1IjoiZWxzb25ueSIsImEiOiJjazkwYWQ2d28wMDJ4M25vNjR3b2h5bWpiIn0.iQk1NtwS-2bJafmWg5Ol9w'
+      }).addTo(leafletMap);         
+      resize();
+  });    
+  const resize = () => {
+      leafletMap.invalidateSize();
+  };
 </script>
 
 <div class="map" bind:this="{mapContainer}"><slot></slot></div>
