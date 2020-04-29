b9e2c65e88ac73e6dbf373462cd0fc032a820d4a
El Sonny <sonny@spaceshiplabs.com>
2020-04-28 19:11:10 -0500

    remove topojson

--- src/LeafletMap.svelte
+++ src/LeafletMap.svelte
@@ -1,17 +1,14 @@
 <script>
-	import mxjson from './mx.json';
 	import L from 'leaflet';
-    import * as topojson from 'topojson';
 	import { onMount, getContext } from 'svelte';
 
-    let mapContainer;    
-    let leafletMap;    
-	
+  let mapContainer;    
+  let leafletMap;    
+
 	export let center = [23.6345, -102.5528];
-    export let zoom = 5;
+  export let zoom = 5;
 
     onMount(() => {
-    	console.log(mxjson);
         leafletMap = L.map(mapContainer).setView(center, zoom);
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: '',
@@ -22,52 +19,10 @@
             accessToken: 'pk.eyJ1IjoiZWxzb25ueSIsImEiOiJjazkwYWQ2d28wMDJ4M25vNjR3b2h5bWpiIn0.iQk1NtwS-2bJafmWg5Ol9w'
         }).addTo(leafletMap);         
         resize();
-        L.topoJson = function (data, options) {
-            return new L.TopoJSON(data, options);
-        };
-      //create an empty geojson layer
-      //with a style and a popup on click
-        var geojson = L.topoJson(null, {
-            style: function(feature){
-              return {
-                color: "#000",
-                opacity: 1,
-                weight: 1,
-                fillColor: '#35495d',
-                fillOpacity: 0.8
-              }
-            },
-            onEachFeature: function(feature, layer) {
-              layer.bindPopup('<p>'+feature.properties.name+'</p>')
-            }
-        }).addTo(leafletMap);
-        geojson.addData(mxjson);
     });    
     const resize = () => {
         leafletMap.invalidateSize();
     };
-
-     //extend Leaflet to create a GeoJSON layer from a TopoJSON file
-    L.TopoJSON = L.GeoJSON.extend({
-        addData: function (data) {
-          var geojson, key;
-          if (data.type === "Topology") {
-            for (key in data.objects) {
-              if (data.objects.hasOwnProperty(key)) {
-                geojson = topojson.feature(data, data.objects[key]);
-                L.GeoJSON.prototype.addData.call(this, geojson);
-              }
-            }
-            return this;
-          }
-          L.GeoJSON.prototype.addData.call(this, data);
-          return this;
-        }
-    });
-      
-    
-
-
 </script>
 
 <div class="map" bind:this="{mapContainer}"><slot></slot></div>
